8a55eac1dc4c49b3bffc7e12bd9db6a72dd7ea3f
Jessie Pearson <223473112+slonepearson@users.noreply.github.com>
2026-07-28 15:01:48 +0200

    feat: added appsettings.json

--- coding-tracker/Program.cs
+++ coding-tracker/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello World")
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+    
+    
+var connectionString = configuration["ConnectionString"];
